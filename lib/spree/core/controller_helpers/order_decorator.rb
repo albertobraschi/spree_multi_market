506128cebf6043bc4eda5191b33d81a8613d08fb
Paolo Chiodi <chiodi84@gmail.com>
2014-07-17 11:01:11 +0200

    make current_market available for everyone

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -1,15 +1,5 @@
 Spree::Core::ControllerHelpers::Order.class_eval do
 
-  def current_market
-    @current_market ||= begin
-      if session.key?(:market) and market = Spree::Market.find(session[:market])
-        market
-      else
-        Spree::Market.default
-      end
-    end
-  end
-
   def current_currency
     @current_currency ||= begin
       if session.key?(:currency) &&
--- lib/spree/core/currency_helpers.rb
+++ lib/spree/core/currency_helpers.rb
@@ -2,6 +2,7 @@ module Spree
   module CurrencyHelpers
     def self.included(receiver)
       receiver.send :helper_method, :supported_currencies
+      receiver.send :helper_method, :current_market
     end
 
     def supported_currencies
@@ -11,14 +12,14 @@ module Spree
     def set_current_market(market_code, currency=nil, overwrite=false)
       if (session[:market].blank? or overwrite) and market = Market.find(market_code)
 
-        @current_market = market_code
+        @current_market = market
         if currency.present? and market.available_currencies.include?(currency)
           @current_currency = currency
         else
           @current_currency = market.available_currencies.first
         end
 
-        session[:market] = @current_market
+        session[:market] = @current_market.code
         session[:currency] = @current_currency
         session[:locale] = market.default_locale
 
@@ -30,12 +31,22 @@ module Spree
       if session[:market].blank?
         market = Market.find_by_countries(countries) || Market.default
 
-        @current_market = market.code
+        @current_market = market
         @current_currency = market.available_currencies.first
 
-        session[:market] = @current_market
+        session[:market] = @current_market.code
         session[:currency] = @current_currency
         session[:locale] = market.default_locale
+      end
+    end
+
+    def current_market
+      @current_market ||= begin
+        if session.key?(:market) and market = Spree::Market.find(session[:market])
+          market
+        else
+          Spree::Market.default
+        end
       end
     end
 
